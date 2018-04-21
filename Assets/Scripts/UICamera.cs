0d4812c2e496d16a6952854a287948c20461526a
Benjamin Hautbois <bhautboi@gmail.com>
2018-04-21 17:46:06 +0200

    Cleanup menu

---
+++ Assets/Scripts/UICamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UICamera : MonoBehaviour {
+    	// Use this for initialization
+	void Start ()
+	{
+		// obtain camera component so we can modify its viewport
+		Camera camera = GetComponent<Camera>();
+        camera.cullingMask = 1 << 8;
+	}
+}
