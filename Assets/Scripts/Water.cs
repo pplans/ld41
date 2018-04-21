5366fc89e3434a83c7c5177826e92336ae6984e5
pplans <pierre.plans@gmail.com>
2018-04-21 18:49:59 +0200

    crappy sampling for normal + pos

--- Assets/Scripts/Water.cs
+++ Assets/Scripts/Water.cs
@@ -33,7 +33,7 @@ public class Water : MonoBehaviour
 		//if (time > Mathf.PI * 2.0f) time = 0.0f;
 	}
 
-	float getHeightAtPoint(Vector2 p)
+	float getHeightAtPoint(Vector2 p, out Vector3 normal)
 	{
 		Mesh mesh = GetComponent<MeshFilter>().mesh;
 		Vector3[] vertices = mesh.vertices;
@@ -54,14 +54,37 @@ public class Water : MonoBehaviour
 		}
 
 		float height = 0.0f;
-		for (int k = 0; k < 3; ++k) height+=nearestPoints[k].y*0.33f;
+		normal.x = normal.y = normal.z = 0.0f;
+		for (int k = 0; k < 3; ++k)
+		{
+			height += nearestPoints[k].y * 0.33f;
+		}
+		normal = Vector3.Cross(nearestPoints[2] - nearestPoints[0], nearestPoints[1] - nearestPoints[0]);
+		Debug.DrawRay(nearestPoints[0], nearestPoints[2] - nearestPoints[0]);
+		Debug.DrawRay(nearestPoints[0], nearestPoints[1] - nearestPoints[0]);
+		Debug.DrawRay(new Vector3(p.x, height, p.y), normal);
+		normal.y = Mathf.Abs(normal.y);
+
 		return height;
 	}
     
     public void GetSurfacePosAndNormalForWPos(Vector3 wpos, out Vector3 outwpos, out Vector3 outwnormal)
     {
-        outwnormal = Vector3.up;
+		float eps = 0.1f;
+		Vector3 p1 = wpos+new Vector3(-eps, 0.0f, 0.00f);
+		Vector3 p2 = wpos + new Vector3(eps, 0.0f, 0.00f);
+		Vector3 p3 = wpos + new Vector3(0.0f, 0.0f, eps);
+
+		outwnormal = Vector3.up;
         outwpos = wpos;
-        outwpos.y = WaterLib.sea_map(wpos, time, sea_choppy, sea_level, sea_speed, sea_freq);
-    }
+        p1.y = WaterLib.sea_map(p1, time, sea_choppy, sea_level, sea_speed, sea_freq);
+		p2.y = WaterLib.sea_map(p2, time, sea_choppy, sea_level, sea_speed, sea_freq);
+		p3.y = WaterLib.sea_map(p3, time, sea_choppy, sea_level, sea_speed, sea_freq);
+		outwnormal = Vector3.Cross(p3-p1, p2-p1);
+		outwnormal.y = Mathf.Abs(outwnormal.y);
+		outwpos.y = 0.33f * (p1.y + p2.y + p3.y);
+
+		/*outwpos = wpos;
+		outwpos.y = getHeightAtPoint(new Vector3(wpos.x, wpos.z), out outwnormal);*/
+	}
 }
